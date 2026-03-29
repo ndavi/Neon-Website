00fdca35d5d09f8991e4fd7c1ad6c2f781aa7d25
Nicolas David <nico01.david@gmail.com>
2026-03-29 19:16:27 +0200

    config: enable native i18n routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,14 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: "fr",
+    locales: ["fr", "en"],
+    routing: {
+      prefixDefaultLocale: true,
+      fallbackType: "redirect"
+    }
+  },
   experimental: {
     svgo: true
   },
